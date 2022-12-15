4e8d3a3b12e22966cb393f929fe96fc33672ef68
Kashall <jordpjones@gmail.com>
2022-12-14 16:16:25 -0800

    Configuration

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export default Object.freeze({
+    RSS_FEED: 'http://feeds.feedburner.com/crunchyroll/rss/anime',
+
+    NAME: 'Crunchy Update',
+
+    PUBLISH_CHANNEL_ID: '1013991782037602477',
+
+    // Keywords you don't want to be updated about.
+    EXCLUDED_KEYWORDS: []
+})
